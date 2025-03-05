74d287678aea96876991ccf131b4a837fc20f52f
YarramallaAjay <yarramallaajay@gmail.com>
2025-03-06 03:39:31 +0530

    added draft routes

---
+++ apps/integrator/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import zod from 'zod';
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+import RegisterRouter from './routes/RegisterRouter';
+import UpdateRouter from './routes/updateRouter';
+
+const router = express.Router();
+
+const client = new PrismaClient();
+
+router.use(express.json());
+router.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true
+}))
+
+router.use("/register",RegisterRouter);
+router.use("/update",UpdateRouter);
