5a71171db7b6033ccb4f29ca3c675789a0a928a8
smit4297 <smitpatel4297@gmail.com>
2025-04-14 02:16:37 +0530

    made changes in configuration

--- apps/zap_backend/src/config.ts
+++ apps/zap_backend/src/config.ts
@@ -2,3 +2,8 @@ export const JWT_SECRET="zapsecret"
 export const GOOGLE_CLIENT_ID="822077570195-ttulc0bbpkg9ibbni70lq2qpdg4eb11u.apps.googleusercontent.com"
 export const GOOGLE_CLIENT_SECRET="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"
 export const GOOGLE_CALLBACK_URL="http://localhost:3000/auth/google/callback"
+
+
+export const GITHUB_CLIENT_ID="Ov23lioP70MvaKLIzy2M"
+export const GITHUB_CLIENT_SECRET="2e57516894812614589c452fe9a857a406ba1a99"
+export const GITHUB_CALLBACK_URL="http://localhost:3000/auth/github/callback"
